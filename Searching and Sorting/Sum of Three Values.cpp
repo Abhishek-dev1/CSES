7dde5fc8c91c91688f8aa319eff6527966056f8a
Abhishek <88185067+Abhishek-dev1@users.noreply.github.com>
2022-09-13 18:52:57 +0530

    Update Sum of Three Values.cpp

--- Searching and Sorting/Sum of Three Values.cpp
+++ Searching and Sorting/Sum of Three Values.cpp
@@ -15,12 +15,13 @@ int main() {
 	}
 	sort(begin(arr), end(arr));
 	for(int i = 0; i < n; i++){
-		int l, r; l = 0; r = n-1;
+		int l, r; 
+		l = 0; r = n-1;
 		while(l != r){
 			int target; target = x - arr[i].first;
 			if(l != i && r != i && arr[l].first + arr[r].first == target){
 				cout << arr[i].second << " " << arr[l].second
-					 << " " << arr.[r].second << endl;
+					 << " " << arr[r].second << endl;
 				return 0;
 			}
 			if(arr[l].first + arr[r].first < target){
